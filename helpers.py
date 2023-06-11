fd69cc39f753d0b1da90e86c11c62a5d6b9dc81c
tracy-st <55551284+tracy-st@users.noreply.github.com>
2023-06-11 11:56:29 -0700

    Update helpers.py
    update keras load_img

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 ##Adapted from https://github.com/YaleDHLab/lab-workshops/tree/master/color-analysis
 
 from keras.preprocessing.image import img_to_array, array_to_img
-from tensorflow.keras.utils import load_img
+from tensorflow.keras.preprocessing.image import load_img
 from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
